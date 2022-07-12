c6b4342b7c50fceb54c6c098e53fa3e8c60275b1
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2022-07-12 21:46:27 +0530

    Update searchingarray.py

--- searchingarray.py
+++ searchingarray.py
@@ -3,11 +3,10 @@ import array as arr
 a = arr.array('i',[])
 user_input = int(input("Enter size of array"))
 
-
 for i in range (user_input):
     value = int(input("Enter Value"))
     a.insert(i,value)
-
+    
 search = int(input("Enter value to search"))
 
 flag=0
@@ -16,16 +15,7 @@ for j in range(user_input):
                 flag=1 
                 # temp=j
                 break
-
 if flag == 1:
     print("value found at ",j)    
 else:
     print("Not Found")
-
-
-
-
-
-
-
-
